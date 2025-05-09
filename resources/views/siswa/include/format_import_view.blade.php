01b3f8da03e4d1d72cbb3a1a17af19e4ef071987
farhanfdlhq <fadilahfarhan80@gmail.com>
2025-05-09 14:09:06 +0700

    update

--- resources/views/siswa/include/format_import_view.blade.php
+++ resources/views/siswa/include/format_import_view.blade.php
@@ -9,20 +9,6 @@
         </tr>
     </thead>
     <tbody>
-        {{-- Contoh Data (Opsional, bisa dikosongkan jika sudah ada dropdown validation) --}}
-        <tr>
-            <td>Budi Santoso</td>
-            <td>1234567890</td>
-            <td>{{ $contohJurusan[0] ?? 'IPA 1' }}</td>
-            <td>{{ $contohKelas[0] ?? 'X IPA 1' }}</td>
-            <td>password123</td>
-        </tr>
-        <tr>
-            <td>Ani Lestari</td>
-            <td>0987654321</td>
-            <td>{{ $contohJurusan[1] ?? 'IPS 2' }}</td>
-            <td>{{ $contohKelas[1] ?? 'XI IPS 2' }}</td>
-            <td>rahasiaku</td>
-        </tr>
+
     </tbody>
 </table>
